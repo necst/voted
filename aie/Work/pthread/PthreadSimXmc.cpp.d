6ec16ce11f37e7971a4e22e2dde1aa1dfda73bf5
giuseppe <gsorrentino@fpga-santa-vm-1.inside.dei.polimi.it>
2025-05-29 06:22:48 +0000

    removed partial output

--- aie/Work/pthread/PthreadSimXmc.cpp.d
+++
@@ -1,4 +0,0 @@
-PthreadSimXmc.cpp.o: PthreadSimXmc.cpp \
- /home/xilinx/software/Vitis/2022.2/aietools/include/adf/x86sim/symbolVisibility.h
-
-/home/xilinx/software/Vitis/2022.2/aietools/include/adf/x86sim/symbolVisibility.h:
