fceae9dc28e9454de1ed5bfa4608d6116c852527
GiuseppeSorrentino99 <giuseppe.sorrentino@polimi.it>
2026-02-09 08:28:46 +0000

    working hw emu hdd-hls design

--- sw/host_code.cpp
+++ sw/host_code.cpp
@@ -1,98 +1,115 @@
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <string>
+#include "../common/common.h"
 #include "experimental/xrt_kernel.h"
 #include "experimental/xrt_uuid.h"
-#include "../common/common.h"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+#include <unistd.h>
 
 // args indexes per kernel
-#define arg_setup_aie_size    0
-#define arg_setup_aie_input   1
-#define arg_sink_from_aie_output 1
-#define arg_sink_from_aie_size   2
+#define arg_mm2s_size 0
+#define arg_mm2s_inA 1
+#define arg_mm2s_inB 2
 
-std::ostream& bold_on(std::ostream& os);
-std::ostream& bold_off(std::ostream& os);
+#define arg_s2mm_output 1
+#define arg_s2mm_size 2
 
-int checkResult(int32_t* input, int32_t* output, int size) {
-    for (int i = 0; i < size; i++) {
-        if (input[i] != output[i]) {
-            std::cout << "Error at index " << i
-                      << ": " << input[i]
-                      << " != " << output[i] << std::endl;
-            return EXIT_FAILURE;
-        }
+std::ostream &bold_on(std::ostream &os);
+std::ostream &bold_off(std::ostream &os);
+
+int checkResult(int32_t *inputA, int32_t *inputB, int32_t *output, int size) {
+  for (int i = 0; i < size; i++) {
+    int val = inputA[i] + inputB[i];
+    if (val != output[i]) {
+      std::cout << "Error at index " << i << ": " << val << " != " << output[i]
+                << std::endl;
+      return EXIT_FAILURE;
     }
-    std::cout << "Test passed!" << std::endl;
-    return EXIT_SUCCESS;
+  }
+  std::cout << "Test passed!" << std::endl;
+  return EXIT_SUCCESS;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <XCLBIN_PATH> [--hw_emu] [DEVICE_ID]" << std::endl;
-        return EXIT_FAILURE;
-    }
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <XCLBIN_PATH> [--hw_emu] [DEVICE_ID]"
+              << std::endl;
+    return EXIT_FAILURE;
+  }
 
-    std::string xclbin_file = argv[1];
+  std::string xclbin_file = argv[1];
 
-    int device_id = 0;
-    if (argc >= 4) {
-        device_id = std::stoi(argv[3]);
-    }
+  int device_id = 0;
+  if (argc >= 4) {
+    device_id = std::stoi(argv[3]);
+  }
 
-    char *env_emu = getenv("XCL_EMULATION_MODE");
-    if (env_emu && std::string(env_emu) == "hw_emu") {
-        std::cout << bold_on << "Program running in hardware emulation mode" << bold_off << std::endl;
-    }
-    else {
-        std::cout << bold_on << "Program running in hardware mode" << bold_off << std::endl;
-    }
+  char *env_emu = getenv("XCL_EMULATION_MODE");
+  if (env_emu && std::string(env_emu) == "hw_emu") {
+    std::cout << bold_on << "Program running in hardware emulation mode"
+              << bold_off << std::endl;
+  } else {
+    std::cout << bold_on << "Program running in hardware mode" << bold_off
+              << std::endl;
+  }
 
-    std::cout << "1. Loading bitstream (" << xclbin_file << ") on device " << device_id << "... ";
-    xrt::device device = xrt::device(device_id);
-    xrt::uuid xclbin_uuid = device.load_xclbin(xclbin_file);
-    std::cout << "Done" << std::endl;
+  std::cout << "1. Loading bitstream (" << xclbin_file << ") on device "
+            << device_id << "... ";
+  xrt::device device = xrt::device(device_id);
+  xrt::uuid xclbin_uuid = device.load_xclbin(xclbin_file);
+  std::cout << "Done" << std::endl;
 
-    xrt::kernel krnl_setup_aie     = xrt::kernel(device, xclbin_uuid, "setup_aie");
-    xrt::kernel krnl_sink_from_aie = xrt::kernel(device, xclbin_uuid, "sink_from_aie");
+  xrt::kernel krnl_mm2s = xrt::kernel(device, xclbin_uuid, "mm2s");
+  xrt::kernel krnl_s2mm = xrt::kernel(device, xclbin_uuid, "s2mm");
 
-    xrtMemoryGroup bank_input  = krnl_setup_aie.group_id(arg_setup_aie_input);
-    xrtMemoryGroup bank_output = krnl_sink_from_aie.group_id(arg_sink_from_aie_output);
+  xrtMemoryGroup bank_inputA = krnl_mm2s.group_id(arg_mm2s_inA);
+  xrtMemoryGroup bank_inputB = krnl_mm2s.group_id(arg_mm2s_inB);
+  xrtMemoryGroup bank_output = krnl_s2mm.group_id(arg_s2mm_output);
 
-    const int32_t size = 32;
-    int32_t nums[size];
-    for (int i = 0; i < size; i++) nums[i] = i + 1;
+  const int32_t size = 128;
+  int32_t numsA[size];
+  int32_t numsB[size];
+  for (int i = 0; i < size; i++) {
+    numsA[i] = 1;
+    numsB[i] = 2;
+  }
 
-    xrt::bo buf_in  = xrt::bo(device, size * sizeof(int32_t), xrt::bo::flags::normal, bank_input);
-    xrt::bo buf_out = xrt::bo(device, size * sizeof(int32_t), xrt::bo::flags::normal, bank_output);
+  xrt::bo buf_inA = xrt::bo(device, size * sizeof(int32_t),
+                            xrt::bo::flags::normal, bank_inputA);
+  xrt::bo buf_inB = xrt::bo(device, size * sizeof(int32_t),
+                            xrt::bo::flags::normal, bank_inputB);
+  xrt::bo buf_out = xrt::bo(device, size * sizeof(int32_t),
+                            xrt::bo::flags::normal, bank_output);
 
-    xrt::run run_setup  = xrt::run(krnl_setup_aie);
-    xrt::run run_sink   = xrt::run(krnl_sink_from_aie);
+  xrt::run run_mm2s = xrt::run(krnl_mm2s);
+  xrt::run run_s2mm = xrt::run(krnl_s2mm);
 
-    run_setup.set_arg(arg_setup_aie_size,  size);
-    run_setup.set_arg(arg_setup_aie_input, buf_in);
+  run_mm2s.set_arg(arg_mm2s_size, size);
+  run_mm2s.set_arg(arg_mm2s_inA, buf_inA);
+  run_mm2s.set_arg(arg_mm2s_inB, buf_inB);
 
-    run_sink.set_arg(arg_sink_from_aie_output, buf_out);
-    run_sink.set_arg(arg_sink_from_aie_size,   size);
+  run_s2mm.set_arg(arg_s2mm_output, buf_out);
+  run_s2mm.set_arg(arg_s2mm_size, size);
 
-    buf_in.write(nums);
-    buf_in.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  buf_inA.write(numsA);
+  buf_inB.write(numsB);
+  buf_inA.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  buf_inB.sync(XCL_BO_SYNC_BO_TO_DEVICE);
 
-    run_sink.start();
-    run_setup.start();
+  run_s2mm.start();
+  run_mm2s.start();
 
-    run_setup.wait();
-    run_sink.wait();
+  run_mm2s.wait();
+  run_s2mm.wait();
 
-    buf_out.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
-    int32_t output_buffer[size];
-    buf_out.read(output_buffer);
+  buf_out.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+  int32_t output_buffer[size];
+  buf_out.read(output_buffer);
 
-    return checkResult(nums, output_buffer, size);
+  return checkResult(numsA, numsB, output_buffer, size);
 }
 
-std::ostream& bold_on(std::ostream& os)  { return os << "\e[1m"; }
-std::ostream& bold_off(std::ostream& os) { return os << "\e[0m"; }
+std::ostream &bold_on(std::ostream &os) { return os << "\e[1m"; }
+std::ostream &bold_off(std::ostream &os) { return os << "\e[0m"; }
